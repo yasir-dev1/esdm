00ab360930336225e0c0b4c8075e131d2f7d352e
yasir-dev1 <112517455+yasir-dev1@users.noreply.github.com>
2023-09-07 16:44:58 +0300

    Add readme to pypi page

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup,find_packages
 
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+
 setup(
     name="esdm",
     version="1.0",
     packages=['esdm'],
+    description='A easy SSH device manger',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     entry_points={
         'console_scripts': [
             'esdm=esdm.__main__:run'
