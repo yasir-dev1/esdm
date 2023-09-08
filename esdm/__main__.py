ca8a5501d54c653a5f7f677f9be74012e62f9c00
yasir-dev1 <112517455+yasir-dev1@users.noreply.github.com>
2023-09-08 12:34:19 +0300

    Adding --copy command to copy files using scp

--- esdm/__main__.py
+++ esdm/__main__.py
@@ -12,6 +12,7 @@ def run():
     parser.add_argument("-a", "--add", help="Add a new SSH device", action="store_true")
     parser.add_argument("-l", "--list", help="List all saved SSH devices", action="store_true")
     parser.add_argument("-d", "--delete", help="Delete a saved SSH device")
+    parser.add_argument("-cp", "--copy", help="Copy a file using SCP")
 
     # Parse the arguments
     args = parser.parse_args()
@@ -65,6 +66,28 @@ def run():
         else:
             print(f"{device_to_delete} is not a saved device.")
 
+    # Copy a file using SCP
+    elif args.copy:
+        device_name = args.remote
+        if device_name in ssh_devices:
+            device_info = ssh_devices[device_name]
+            ip = device_info["ip"]
+            username = device_info["username"]
+            password = device_info["password"]
+            source_file = args.copy
+            destination = input("Enter the destination directory: ")
+
+            # Create the SCP command
+            scp_command = f"sshpass -p {password} scp {source_file} {username}@{ip}:{destination}"
+
+            try:
+                subprocess.run(scp_command, shell=True, check=True)
+                print(f"File {source_file} copied to {device_name}:{destination}")
+            except subprocess.CalledProcessError as e:
+                print(f"SCP operation failed: {e}")
+        else:
+            print(f"{device_name} is not a saved device.")
+
     # Connect to a remote device
     elif args.remote in ssh_devices:
         device_info = ssh_devices[args.remote]
@@ -73,7 +96,7 @@ def run():
         password = device_info["password"]
 
         # Create the SSH connection
-        ssh_command = f"sshpass -p {password} ssh {username}@{ ip}"
+        ssh_command = f"sshpass -p {password} ssh {username}@{ip}"
         
         try:
             subprocess.run(ssh_command, shell=True, check=True)
